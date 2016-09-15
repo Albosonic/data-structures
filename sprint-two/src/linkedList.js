c52e183d08b92ae6874c6a3db78e33b62556a2fd
Albosonic <albertomadueno@gmail.com>
2016-09-15 09:15:51 -0700

    Passed: LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+    }
+    if (list.tail !== null) {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var prevHead = this.head;
+    this.head = this.head.next;
+    return prevHead.value;
   };
 
   list.contains = function(target) {
+    var found = false;
+    var pointer = this.head;
+
+    while (pointer !== null && !found) {
+      if (pointer.value === target) {
+        found = true;
+      }
+      pointer = pointer.next;
+    }
+
+    return found;
   };
 
   return list;
