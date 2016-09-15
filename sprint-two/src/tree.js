36c8d14cfb3814e4bdcfc07a68dc2c8a78b9b64d
Albosonic <albertomadueno@gmail.com>
2016-09-15 09:33:50 -0700

    Passing: Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,35 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var found = false;
+
+  var subroutine = function (node) {
+    if (node.value === target) {
+      found = true;
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      if (node.children[i].value === target) {
+        found = true;
+      }
+      if (node.children[i]) {
+        subroutine(node.children[i]);
+      }
+    }
+  };
+
+  subroutine(this);
+  return found;
 };
 
 
